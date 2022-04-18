bd6cc8902e0c2a93f6845e8c42fc6d29770408c2
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-18 00:28:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,14 @@ st.button('Decrement', on_click=decrement_counter,
 
 st.write('Count = ', st.session_state.count)
 
+import streamlit as st
+
+st.title('Counter Example')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+st.write('Count = ', st.session_state.count)
