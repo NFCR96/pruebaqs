f88e16372ccc7623e02ce5255a1621801eed7033
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-18 00:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ st.button('Decrement', on_click=decrement_counter,
 
 st.write('Count = ', st.session_state.count)
 
-import streamlit as st
 
-st.title('Counter Example')
+st.title('Counter Example using Callbacks')
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
-increment = st.button('Increment')
-if increment:
+def increment_counter():
     st.session_state.count += 1
+
+st.button('Increment', on_click=increment_counter)
 
 st.write('Count = ', st.session_state.count)
