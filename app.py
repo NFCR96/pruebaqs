10b4c1ee689d4f1cd3293f97275fdd8b5db94e18
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-24 17:52:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Berrugas', 'Secrecion', 'Dolor', 'enrojecimiento'],
-     ['Comezon', 'Picazon'])
-if 'Berrugas' st.write('Secrecion')
-
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
    
